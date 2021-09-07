93e63f228779503332ef5835db1371466ee72d8d
Jonas <web@emd.art.br>
2021-09-07 04:04:49 -0300

    class components with states

--- src/App.js
+++ src/App.js
@@ -3,19 +3,37 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
+  state = {
+      name: 'Agora to aprendendo',
+      counter: 0
+    };
+  
+  handlePClick = () => {    
+    this.setState( { name: 'Salve Popotooo!!'} );
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const {counter} = this.state;
+    this.setState({ counter: counter + 1});
+  }
+
   render() {
+    const { name, counter } = this.state;
+    
     return (
       <div className = "App" >
         <header className = "App-header">
         <img src = {logo} className = "App-logo" alt = "logo" />
-        <p>
-          Edit <code>src/App.js</code>and save to reload.
+        <p onClick={this.handlePClick}>
+          {name} {counter}
         </p>
           <a
-      className = "App-link"
-      href = "https://reactjs.org"
-      target = "_blank"
-      rel = "noopener noreferrer" >
+          onClick={this.handleAClick}
+          className = "App-link"
+          href = "https://reactjs.org"
+          target = "_blank"
+          rel = "noopener noreferrer" >
         Learn React
         </a>
         </header>
