f1e11bbc9ad583d0bf2ed1124ffe1a6ace73afd1
Jonas <web@emd.art.br>
2021-09-09 02:13:53 -0300

    search of external api

--- src/App.js
+++ src/App.js
@@ -1,70 +1,52 @@
-import './App.css';
-import {
-  Component
-} from 'react';
+import "./App.css";
+import { Component } from "react";
+
+// Class Components // 
 
 class App extends Component {
-  state = {
-    counter: 0,
-    posts: [{
-        id: 1,
-        title: 'O titulo 1',
-        body: 'O corpo'
-      },
-      {
-        id: 2,
-        title: 'O titulo 2',
-        body: 'O corpo'
-      },
-      {
-        id: 3,
-        title: 'O titulo 3',
-        body: 'O corpo'
-      }
-    ]
+  state = {    
+    posts: []
   };
 
-  // Começando o ciclo de vida dos componentes  
+  // Começando o ciclo de vida dos componentes
 
   componentDidMount() {
-    this.handleTimeout();    
-  }
-  timeoutUpdate = null;
+    fetch("https://jsonplaceholder.typicode.com/albums")
+      .then((response) => response.json())
+      .then((posts) => this.setState({ posts }));
 
-  componentDidUpdate() {    
-    this.handleTimeout();
+    
   }
-
-  componentWillUnmount() {
-    clearTimeout(this.timeoutUpdate);
-  } 
-  // Termina o ciclo de vida dos componentes  
+  
+  // Termina o ciclo de vida dos componentes
 
   handleTimeout = () => {
     const { posts, counter } = this.state;
-    posts[0].title = 'O titulo mudou';        
+    posts[0].title = "O titulo mudou";
 
     this.timeoutUpdate = setTimeout(() => {
       this.setState({
         posts,
-        counter: counter + 1
+        counter: counter + 1,
       });
-    }, 1000)
-  }
+    }, 1000);
+  };
+
+  // Componente //
 
   render() {
-    const { posts, counter} = this.state;   
+    const { posts, counter } = this.state;
 
-    return (<div className = "App">
-        <h1> {counter} </h1> {
-        posts.map(post => (<div key = {post.id}>
-          <h1> {post.title} </h1> <p> {post.body}</p>
-           </div>
-        ))
-      } </div>
-
-  )
+    return (
+      <div className="App">
+        <h1> {counter} </h1>{" "}
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1> {post.title} </h1> <p> {post.body}</p>
+          </div>  
+        ))}{" "}
+      </div>
+    );
+  }
 }
-}
-
 export default App;
