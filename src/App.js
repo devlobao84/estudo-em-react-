fed00f508d0c10d73561bab64b89ff7cc0cc6832
Lobao <web@emd.art.br>
2021-09-16 00:11:47 -0300

    consumindo api v0.1

--- src/App.js
+++ src/App.js
@@ -1,70 +1,77 @@
-import './App.css';
-import {
-  Component
-} from 'react';
+import "./App.css";
+import { Component } from "react";
 
 class App extends Component {
   state = {
     counter: 0,
-    posts: [{
+    posts: [
+      {
         id: 1,
-        title: 'O titulo 1',
-        body: 'O corpo'
+        title: "O titulo 1",
+        body: "O corpo",
       },
       {
         id: 2,
-        title: 'O titulo 2',
-        body: 'O corpo'
+        title: "O titulo 2",
+        body: "O corpo",
       },
       {
         id: 3,
-        title: 'O titulo 3',
-        body: 'O corpo'
-      }
-    ]
+        title: "O titulo 3",
+        body: "O corpo",
+      },
+    ],
   };
 
-  // Começando o ciclo de vida dos componentes  
+  // Começando o ciclo de vida dos componentes
 
   componentDidMount() {
-    this.handleTimeout();    
-  }
-  timeoutUpdate = null;
-
-  componentDidUpdate() {    
-    this.handleTimeout();
+    this.loadPosts();
   }
 
-  componentWillUnmount() {
-    clearTimeout(this.timeoutUpdate);
-  } 
-  // Termina o ciclo de vida dos componentes  
+  loadPosts = async () => {
+    const postResponse = fetch("https://jsonplaceholder.typicode.com/posts");
+
+    const [posts] = await Promise.all([postResponse]);
+
+    const postsJson = await posts.json();
+
+    this.setState({ posts: postsJson });
+  };
+
+  // Termina o ciclo de vida dos componentes
 
   handleTimeout = () => {
     const { posts, counter } = this.state;
-    posts[0].title = 'O titulo mudou';        
+    posts[0].title = "O titulo mudou";
 
     this.timeoutUpdate = setTimeout(() => {
       this.setState({
         posts,
-        counter: counter + 1
+        counter: counter + 1,
       });
-    }, 1000)
-  }
+    }, 1000);
+  };
 
   render() {
-    const { posts, counter} = this.state;   
+    const { posts, counter } = this.state;
 
-    return (<div className = "App">
-        <h1> {counter} </h1> {
-        posts.map(post => (<div key = {post.id}>
-          <h1> {post.title} </h1> <p> {post.body}</p>
-           </div>
-        ))
-      } </div>
-
-  )
-}
+    return (
+      <section className="container">
+        <div className="posts">
+          <h1> {counter} </h1>{" "}
+          {posts.map((post) => (
+            <div className="post">
+            
+            <div key={post.id} className="post-content">
+              <h1> {post.title} </h1> <p> {post.body}</p>
+            </div>
+            </div>
+          ))}{" "}
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
