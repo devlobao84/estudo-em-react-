0ae803f5ec3b64e33b16b987be9d4c97fa0c12d3
Jonas <web@emd.art.br>
2021-10-31 17:37:06 -0300

    pagination by post and id- v.0.1

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -2,7 +2,8 @@ export const PostCard = ({ title, cover, body, id}) => (
         <div className="post">
               <img src={cover} alt={title} />
               <div key={id} className="post-content">
-                <h1> {title} </h1> <p> {body}</p>
+                <h1> {title} {id}</h1>
+                 <p> {body}</p>
               </div>
             </div>
     );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,11 +4,15 @@ import "./styles.css";
 
 import { Posts } from '../../components/Posts';
 import { loadPosts } from '../../utils/load-posts';
+import { Button } from "../../components/Button";
 
 // Class Components //
 export class Home extends Component {
   state = {
     posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 2
   };
 
   // Fazendo uma requisição de API externa
@@ -17,13 +21,29 @@ export class Home extends Component {
   }
 
   loadPosts = async () => {
+    const { page, postsPerPage} = this.state
     const postsAndPhotos = await loadPosts();
-    this.setState({ posts: postsAndPhotos });
+    this.setState({
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos,
+    });
   };
 
-  // Termina aqui o ciclo de vida dos componentes
+  loadMorePosts = () => {
+    const {
+      page,
+      postsPerPage,
+      allPosts,
+      posts
+    } = this.state;
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
 
-  // Componente refatorado //
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page:nextPage})
+   ;
+  }
 
   render() {
     const { posts } = this.state;
@@ -31,6 +51,10 @@ export class Home extends Component {
     return (
       <section className="container">
         <Posts posts={posts} />
+        <Button
+         text="Algo a mais"
+         onClick={this.loadMorePosts}
+          />       
       </section>
     );
   }
