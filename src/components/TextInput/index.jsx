4041a546586600bc5db644b9027804d933963103
Jonas <web@emd.art.br>
2021-11-21 23:32:06 -0300

    style input fish

--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -2,6 +2,12 @@ import './styles.css'
 
 export const TextInput = ({ searchValue, handleChange  }) => {
   return<div className="inputsearch">
-   <input className="input" onChange={handleChange} value={searchValue} type="search" />
+   <input
+    className="input"
+    onChange={handleChange}
+    value={searchValue} 
+    type="search"
+    placeholder="Type yout search"
+     />
   </div>
 };
