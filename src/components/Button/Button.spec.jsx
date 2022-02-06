de8d9f0b8e343e9a8791062b0eff18b3f0221317
Jonas <web@emd.art.br>
2022-02-06 03:29:30 -0300

    corrections from previous tests

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -2,13 +2,15 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Button } from ".";
 
-describe("<button />", () => {
+describe("<Button />", () => {
   it('should render the button with the text "Load More', () => {
     render(<Button text="Load More" />);
     expect.assertions(1);
     const button = screen.getByRole("button", { name: /load more/i });
     expect(button).toBeInTheDocument();
   });
+
+  // with onClick and jest.fn // 
 
   it("should call function on button click", () => {
     const fn = jest.fn();
@@ -18,11 +20,13 @@ describe("<button />", () => {
     expect(fn).toHaveBeenCalledTimes(1);
   });
 
-  //it("shound be disabled when disabled is true", () => {    
-  //  render(<Button text="Load more" disabled={true} />);
-  //  const button = screen.getByRole("button", { name: /load more/i });
-  //  expect(button).toBeEnabled();
-  //});
+  // testing with disabled // 
+
+  it("shound be disabled when disabled is true", () => {    
+    render(<Button text="Load more" disabled={true} />);
+    const button = screen.getByRole("button", { name: /load more/i });
+    expect(button).toBeDisabled();
+  });
 
   it("shound be enabled when disabled is false", () => {
     const fn = jest.fn();
--- src/pages/Home/Home.test.jsx
+++ src/pages/Home/Home.test.jsx
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
+
 describe('<Home />', () => {
-  it("is a dommy test", () => {
-=======
-describe ('<Home />', () => {
-  
-  it('its a dummy test', () => {
->>>>>>> 690d07f0ecaacf31bd3425b6caf92e5410511ec2
-    expect(1).toBe(1);
+  it("is a doummy test", () => {
+    expect(1).toBe(1); 
   });
 });
-
